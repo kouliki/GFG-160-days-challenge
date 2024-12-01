91963eb0cf302bf0575a71a5c6176fcbe11554e4
Kouliki Mahato <95127992+kouliki@users.noreply.github.com>
2024-12-01 22:48:13 +0530

    Update day_1.py

--- Days/day_1.py
+++ Days/day_1.py
@@ -1,1 +1,16 @@
+# Second Largest
+
+class Solution:
+    def getSecondLargest(self, arr):
+        # Code Here
+        if len(arr)<2:
+            return -1
+        a=list(set(arr))
+        if len(a)<2:
+            return -1
+        
+        
+        a.sort()
+        return a[-2]
+
 

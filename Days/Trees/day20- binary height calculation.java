f1376181687c3b9f4c99314cc281a6f40fc7f01d
Kouliki Mahato <95127992+kouliki@users.noreply.github.com>
2025-03-06 10:35:51 +0530

    Create day20- binary height calculation.java

---
+++ Days/Trees/day20- binary height calculation.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Function to find the height of a binary tree.
+    int height(Node node) {
+        // code here
+        
+        if(node==null){
+            return -1;
+        }
+        int leftHeight=height(node.left);
+        int rightHeight=height(node.right);
+        
+        return 1 +Math.max(leftHeight,rightHeight);
+        
+        
+    }
+}
